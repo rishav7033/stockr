c2323a6ab72768c8c38826d10e513caf6a464c1c
Hackrhive <hivethehacker@gmail.com>
2019-10-17 07:52:38 +0530

    updated

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,143 +23,145 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 24.0, bottom: 24),
-            child: Container(
-              width: double.infinity,
-              child: Text(
-                'STOCKS',
-                style: TextStyle(
-                  fontFamily: 'Freder',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.blueGrey,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ITCompanies()));
-              });
-            },
-            child: Container(
-              child: Center(
-                child: Text(
-                  'IT',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'Freder',
+      body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 24.0, bottom: 24),
+                child: Container(
+                  width: double.infinity,
+                  child: Text(
+                    'STOCKS',
+                    style: TextStyle(
+                      fontFamily: 'Freder',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.blueGrey,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
                 ),
               ),
-              margin: EdgeInsets.all(15),
-              height: 100.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Color(0xcc49547A),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => AutoCorps()));
-              });
-            },
-            child: Container(
-              child: Center(
-                child: Text(
-                  'AUTOCORPS',
-                  style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
-                  textAlign: TextAlign.center,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => ITCompanies()));
+                  });
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'IT',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Freder',
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  margin: EdgeInsets.all(15),
+                  height: 100.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Color(0xcc49547A),
+                      borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              margin: EdgeInsets.all(15),
-              height: 100.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Color(0xcc49547A),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Food()));
-              });
-            },
-            child: Container(
-              child: Center(
-                child: Text(
-                  'FOOD',
-                  style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
-                  textAlign: TextAlign.center,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AutoCorps()));
+                  });
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'AUTOCORPS',
+                      style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  margin: EdgeInsets.all(15),
+                  height: 100.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Color(0xcc49547A),
+                      borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              margin: EdgeInsets.all(15),
-              height: 100.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Color(0xcc49547A),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Bank()));
-              });
-            },
-            child: Container(
-              child: Center(
-                child: Text(
-                  'BANKING',
-                  style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
-                  textAlign: TextAlign.center,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => Food()));
+                  });
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'FOOD',
+                      style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  margin: EdgeInsets.all(15),
+                  height: 100.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Color(0xcc49547A),
+                      borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              margin: EdgeInsets.all(15),
-              height: 100.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Color(0xcc49547A),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Fashion()));
-              });
-            },
-            child: Container(
-              child: Center(
-                child: Text(
-                  'Fashion',
-                  style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
-                  textAlign: TextAlign.center,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => Bank()));
+                  });
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'BANKING',
+                      style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  margin: EdgeInsets.all(15),
+                  height: 100.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Color(0xcc49547A),
+                      borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              margin: EdgeInsets.all(15),
-              height: 100.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Color(0xcc49547A),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Fashion()));
+                  });
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'Fashion',
+                      style: TextStyle(fontSize: 30, fontFamily: 'Freder'),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  margin: EdgeInsets.all(15),
+                  height: 100.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Color(0xcc49547A),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ],
           ),
-        ],
       ),
     );
   }
